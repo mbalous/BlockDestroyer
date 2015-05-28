1d1f6e1fdb24eb1b4a3d7f300146f9f002a8da09
oxxidQ <m.balouse@gmail.com>
2015-05-28 15:51:46 +0200

    Ball class

---
+++ BlockDestroyer/BallClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BlockDestroyer
+{
+    class BallClass : GameObject
+    {
+        public char BallChar { get; set; }
+        public bool GoingUp { get; set; }
+        public bool GoingRight { get; set; }
+        public Direction Dir { get; set; }
+
+        public BallClass(int xPosition, int yPosition, bool goingUp, bool goingRight, char ballChar, bool exists = true, ConsoleColor color = ConsoleColor.White)
+            : base(xPosition, yPosition, exists, color)
+        {
+            GoingUp = goingUp;
+            GoingRight = goingRight;
+            BallChar = ballChar;
+        }
+
+        public BallClass(int xPosition, int yPosition, bool exists, Direction dir, char ballChar, ConsoleColor color = ConsoleColor.White)
+            : base(xPosition, yPosition, exists, color)
+        {
+            this.Dir = dir;
+            this.BallChar = ballChar;
+        }
+    }
+
+    abstract class Direction
+    {
+
+    }
+
+    class UpRight : Direction
+    {
+
+    }
+
+    class UpLeft : Direction
+    {
+
+    }
+
+    class DownRight : Direction
+    {
+
+    }
+
+    class DownLeft : Direction
+    {
+
+    }
+}
